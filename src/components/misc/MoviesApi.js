ac74f9c7dc181b6dea2e772fffa16fdd83dfa5b4
skyglass <skyglass2001@gmail.com>
2023-07-30 01:53:34 +0200

    test

--- src/components/misc/MoviesApi.js
+++ src/components/misc/MoviesApi.js
@@ -12,39 +12,39 @@ export const moviesApi = {
 }
 
 function getMovies() {
-  return instance.get('/')
+  return instance.get('/movie')
 }
 
 function getMovie(imdbId) {
-  return instance.get(`/${imdbId}`)
+  return instance.get(`/movie/${imdbId}`)
 }
 
 function saveMovie(movie, token) {
-  return instance.post('/', movie, {
+  return instance.post('/movie', movie, {
     headers: { 'Authorization': bearerAuth(token) }
   })
 }
 
 function deleteMovie(imdbId, token) {
-  return instance.delete(`/${imdbId}`, {
+  return instance.delete(`/movie/${imdbId}`, {
     headers: { 'Authorization': bearerAuth(token) }
   })
 }
 
 function addMovieComment(imdbId, comment, token) {
-  return instance.post(`/${imdbId}/comments`, comment, {
+  return instance.post(`/movie/${imdbId}/comments`, comment, {
     headers: { 'Authorization': bearerAuth(token) }
   })
 }
 
 function getUserExtrasMe(token) {
-  return instance.get(`/userextras/me`, {
+  return instance.get(`/movie/userextras/me`, {
     headers: { 'Authorization': bearerAuth(token) }
   })
 }
 
 function saveUserExtrasMe(token, userExtra) {
-  return instance.post(`/userextras/me`, userExtra, {
+  return instance.post(`/movie/userextras/me`, userExtra, {
     headers: { 'Authorization': bearerAuth(token) }
   })
 }
